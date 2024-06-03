d8c32f619dbf51872eb4cc96de19cf10cba0e7ac
Ian1274 <115207282+Ian1274@users.noreply.github.com>
2024-06-03 13:30:58 +0800

    Add files via upload

---
+++ AI01_rlGame/test.py
@@ -0,0 +1,25 @@
+# 导入相关的库
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+
+# 创建环境
+env = gym_super_mario_bros.make('SuperMarioBros-2-2-v0')
+
+# 导入动作集：COMPLEX_MOVEMENT，包含了所有的动作
+# 将动作集应用到环境中
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
+
+# 重置环境
+state = env.reset()
+for step in range(5000):
+    # 渲染游戏画面
+    env.render()
+    # 获取游戏的状态，奖励，是否结束，额外信息
+    state, reward, done, info = env.step(env.action_space.sample())
+    # 如果游戏结束，那么重置游戏
+    if done:
+        state = env.reset()
+
+# 关闭环境
+env.close()
